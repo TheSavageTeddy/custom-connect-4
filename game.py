92793a4ff05b86d605d80c98d5af6e3c940bba4b
TheSavageTeddy <51810476+TheSavageTeddy@users.noreply.github.com>
2023-04-10 22:22:24 +0800

    final bug fixes, commenting, remove debug

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ class Colors:
     """
     ANSI color codes
     Source: https://gist.github.com/rene-d/9e584a7dd2935d0f461904b9f2950007
+
+    These are purely cosmetic, adding colours to outputted text to enhance
+    the game's graphics within the terminal
     """
     '''
     THESE ARE ALL CONSTANT VARIABLES, THEY DO NOT CHANGE, ONLY ACCESSED
@@ -46,7 +49,8 @@ class Colors:
 
 
 
-class connectFour:
+class connectFour: # Module for the connect 4 logic and helper functions and components of the game
+    # Executed when the class is called, sets up important game variables such as the board, dimensions, players, scores
     def __init__(self, boardHeight: int, boardWidth: int, players: int, terminalWidth: int, terminalHeight:int, scores: dict) -> None:
         self.height = boardHeight
         self.width = boardWidth
@@ -68,6 +72,9 @@ class connectFour:
 
         self.terminalWidth, self.terminalHeight = terminalWidth, terminalHeight
 
+    # A helper function to center a string containing ANSI codes, 
+    # as normal python format strings will consider ANSI characters
+    # into the length, thus centering incorrectly
     def center_ansi_text(self, string: str, width: int, char: str = " ") -> str:
         ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])') # https://stackoverflow.com/questions/14693701/how-can-i-remove-the-ansi-escape-sequences-from-a-string-in-python
         true_string_length = len(ansi_escape.sub('', string)) # actual visual length of string - disregards ansi colouring codes
@@ -75,20 +82,25 @@ class connectFour:
         left = width - right - true_string_length
         return f'''{right * char}{string}{left * char}'''
 
+    # Print out the current scoreboard, with the highest scoring player(s) at the top
     def print_scoreboard(self):
+        # Sort the scoreboard from highest score to lowest score
         sorted_scoreboard = [[player, score] for player, score in self.scoreboard.items()]
         sorted_scoreboard = sorted(sorted_scoreboard, key=lambda x: x[1], reverse=True)
+
         print()
         print(Colors.UNDERLINE + Colors.BOLD + f"    SCOREBOARD    " + Colors.END)
         print()
+
         prev_score = -1 # set to something the score can't be (gets ignored first loop)
         prev_placement = -1 # set to something the placement can't be (gets ignored first loop)
         for i, scorecard in enumerate(sorted_scoreboard):
             player, score = scorecard
             placement_colour = Colors.END + Colors.DARK_GRAY
             placement = i + 1
-            if prev_score == score:
+            if prev_score == score: # Take into account where 2 or more player's score are the same
                 placement = prev_placement
+            # Highlight placement based on traditional medal colours (bronze, silver, gold)
             match placement: 
                 case 1: # gold/yellow
                     placement_colour = Colors.YELLOW + Colors.BOLD
@@ -101,8 +113,10 @@ class connectFour:
             prev_placement = placement
         print() # seperate scoreboard from next print
 
+    # Update the scoreboard when the game ends.
+    # If winning player is None, that represents the event of a draw
     def update_scoreboard(self, winning_player):
-        if winning_player == None:
+        if winning_player == None: # Event of a draw
             for player in self.scoreboard:
                 print(Colors.CYAN + Colors.BOLD + f"For DRAW: Player {self.scoreboard[player]} recieves 1.5 points ({self.scoreboard[player]} -> {self.scoreboard[player]+1.5})" + Colors.END)
                 self.scoreboard[player] += 1.5
@@ -110,10 +124,11 @@ class connectFour:
 
         winner = winning_player
         losers = []
-        for player in self.players:
+        for player in self.players: # Losers is everyone that isn't the winner
             if player != winner:
                 losers.append(player)
 
+        # Updates scores accordingly, and displays the change in score.
         print(Colors.GREEN + Colors.BOLD + f"For WIN: Player {winner} recieves 2 points ({self.scoreboard[winner]} -> {self.scoreboard[winner]+2})" + Colors.END)
         self.scoreboard[winner] += 2
         for loser in losers:
@@ -122,6 +137,7 @@ class connectFour:
           
         return self.scoreboard
     
+    # Function to colour the markers of the board to the player's colour
     def colour_markers(self, string: str) -> str:
         colouredString = ""
         for i, char in enumerate(string):
@@ -135,28 +151,26 @@ class connectFour:
                         colouredString += char
         return colouredString
 
+    # Prints out the game board, as well as the numbers at the bottom displaying the column number
     def display_board(self) -> None:
         for row in self.board:
             print(f'{self.center_ansi_text(self.colour_markers(" ".join(map(str, row))), self.terminalWidth)}')
         
         print(f'{self.center_ansi_text(Colors.BOLD + " ".join(map(str, range(1, self.width + 1))) + Colors.END, self.terminalWidth)}')
 
+    # Flips a 2 dimensional (rectangular) array (e.g. [row][col] -> [col][row])
     def flip_dimension(self, arr: list) -> list:
-        '''
-        Flips a 2 dimensional array
-        '''
         return [[arr[i][e] for i in range(len(arr))] for e in range(len(arr[0]))]
 
+    # Check if the board state is a draw (no available spots to place a marker)
     def check_draw(self) -> bool:
         return not any([item == 0 for row in self.board for item in row])
 
+    # Check if a player has won, given the board and the player number
     def check_win(self, player) -> bool:
-        '''
-        Check if a player has won, given the board and the player number
-        '''
 
         winning_string = str(player) * 4
-
+        
         # check rows and columns
         if any([winning_string in "".join(map(str, row)) for row in self.board]):
             return True
@@ -166,23 +180,21 @@ class connectFour:
         # check left down to right up diagonal
         for row in range(3, self.height):
             for col in range(0, self.width - 3):
-                #print(f'''{row = }\n{col = }\n''')
                 if winning_string == "".join(map(str, [self.board[row][col], self.board[row-1][col+1], self.board[row-2][col+2], self.board[row-3][col+3]])):
                     return True
         
         # check left up to right down diagonal
         for row in range(3, self.height):
             for col in range(3, self.width):
-                print(f'''{row = }\n{col = }''')
-                print("".join(map(str, [self.board[row][col], self.board[row-1][col-1], self.board[row-2][col-2], self.board[row-3][col-3]])))
-                print()
                 if winning_string == "".join(map(str, [self.board[row][col], self.board[row-1][col-1], self.board[row-2][col-2], self.board[row-3][col-3]])):
                     return True
-        
+        # If none of the above matched, there is no win
         return False
 
+    # Check if the specified column is a valid move (there exists a spot where the marker can go)
     def valid_move(self, column) -> bool:
         return 0 in self.flip_dimension(self.board)[column - 1]
 
+    # Update the board given a player's move in a given column
     def make_move(self, column, player):
         self.board["".join(map(str, self.flip_dimension(self.board)[column-1])).rindex("0")][column-1] = player
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ terminalWidth, terminalHeight = shutil.get_terminal_size()
 Error handling functions
 '''
 
+# Gets a number from the user, given the minumum and maximum number it can be
+# blankAllowed for the case where the user can enter nothing for the previously set value
+# Keeps prompting until user enters a valid input
 def getNumber(prompt, minimum = 0, maximum = None, blankAllowed = False) -> int:
     a = input(prompt)
     while True:
@@ -22,6 +25,8 @@ def getNumber(prompt, minimum = 0, maximum = None, blankAllowed = False) -> int:
         print(Colors.RED + "Invalid number!" + Colors.END)
         a = input(prompt)
 
+# Gets a yes or no from the user
+# Keeps prompting until user enters a valid input
 def getYesNo(prompt) -> bool:
     a = input(prompt)
     while True:
@@ -34,35 +39,30 @@ def getYesNo(prompt) -> bool:
                 print(Colors.RED + "Invalid input! Please enter 'yes' or 'no'" + Colors.END)
         a = input(prompt)
 
+# Main function of the game
 def main():
     gameFinished = False
     scoreboard = []
     
+    # Prompt user for the customizable game settings
     boardHeight = getNumber("Enter board height (min 4): ", minimum=4)
     boardWidth = getNumber("Enter board width (min 4): ", minimum=4)
     players = getNumber("Enter amount of players (min 2, max 8): ", minimum=2)
     
     while True:
+        # Initialize the game
         game = connectFour(boardHeight, boardWidth, players, terminalWidth, terminalHeight, scoreboard)
         while True:
-            for player in range(1, players+1):
+            for player in range(1, players+1): # Each player takes a turn in placing a marker
                 game.display_board()
-                while True:
-                    column = getNumber(f"Player {player}'s move: ", minimum=1, maximum=boardWidth)
+                while True: # Make sure the move they enter is valid
+                    column = getNumber(f"{game.players[player]['colour']}Player {player}'s move: {Colors.END}", minimum=1, maximum=boardWidth)
                     if game.valid_move(column):
                         game.make_move(column, player)
                         break
                     else:
                         print(Colors.RED + "Invalid column!" + Colors.END)
-                if game.check_draw():
-                    game.display_board()
-                    print(Colors.LIGHT_GRAY + f"It's a Draw!")
-                    scoreboard = game.update_scoreboard(None)
-                    input(Colors.ITALIC + "Press enter to continue to scoreboard..." + Colors.END)
-                    game.print_scoreboard()
-                    gameFinished = True
-                    break
-                if game.check_win(player):
+                if game.check_win(player): # Check if the player that just placed their marker has won
                     game.display_board()
                     print(Colors.YELLOW + f"Player {player} wins!")
                     scoreboard = game.update_scoreboard(player)
@@ -70,14 +70,23 @@ def main():
                     game.print_scoreboard()
                     gameFinished = True
                     break
+                if game.check_draw(): # Check if its a Draw
+                    game.display_board()
+                    print(Colors.LIGHT_GRAY + f"It's a Draw!")
+                    scoreboard = game.update_scoreboard(None)
+                    input(Colors.ITALIC + "Press enter to continue to scoreboard..." + Colors.END)
+                    game.print_scoreboard()
+                    gameFinished = True
+                    break
             if gameFinished:
-                break
+                break # This break is required as the previous breaks out of the for loop
         
         gameFinished = False
         if not getYesNo(Colors.LIGHT_PURPLE + "Do you want to play again? (Yes/No) " + Colors.END):
             print("Exiting...")
             exit()
         
+        # Option to re-enter game settings after the game has ended
         print("Please re-enter game settings, leave blank to use previous settings.")
         newBoardHeight = getNumber("Enter board height (min 4): ", minimum=4, blankAllowed=True)
         newBoardWidth = getNumber("Enter board width (min 4): ", minimum=4, blankAllowed=True)
